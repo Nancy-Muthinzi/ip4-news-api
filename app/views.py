78ea5c1ba1a7677acfbf357c9ea967b1489f09bc
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-08-30 18:27:47 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Welcome to the most informative news site online!!!'
+    return render_template('index.html', title = title)
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    return render_template('news.html',id = news_id)
