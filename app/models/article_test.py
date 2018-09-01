84274f63255006a261141621350f564b4c777027
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-09-01 20:58:27 +0300

    create API request for articles

--- app/models/article_test.py
+++ app/models/article_test.py
@@ -1,21 +1,21 @@
-# import unittest
-# from models import news_article
+import unittest
+from models import news_article
 
-# Article = article.Article
+Article = article.Article
 
-# class ArticleTest(unittest.TestCase):
-#     '''
-#     Test class to test the behaviour of the news articles class
-#     '''
+class ArticleTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the news articles class
+    '''
 
-#     def setUp(self):
-#         '''
-#         Method to run before every test
-#         '''
-#         self.new_article = Article('the-new-york-times', 'The New York Times', 'Julie Hirschfeld Davis', 'Washington Prepares to Honor John McCain: The Senator Is to Lie in State in US Capitol', 'Here’s what to expect as a series of tributes to the senator begin to unfold in Washington.', 'https://www.nytimes.com/2018/08/31/us/politics/john-mccain-funeral-capitol.html', 'https://static01.nyt.com/images/2018/08/31/us/politics/31dc-mccainbrief/merlin_143016915_094b89e4-a509-4b9f-b4a3-ee683cdaf179-facebookJumbo.jpg', '2018-08-31T11:00:03Z')
+    def setUp(self):
+        '''
+        Method to run before every test
+        '''
+        self.new_article = Article('the-new-york-times', 'The New York Times', 'Julie Hirschfeld Davis', 'Washington Prepares to Honor John McCain: The Senator Is to Lie in State in US Capitol', 'Here’s what to expect as a series of tributes to the senator begin to unfold in Washington.', 'https://www.nytimes.com/2018/08/31/us/politics/john-mccain-funeral-capitol.html', 'https://static01.nyt.com/images/2018/08/31/us/politics/31dc-mccainbrief/merlin_143016915_094b89e4-a509-4b9f-b4a3-ee683cdaf179-facebookJumbo.jpg', '2018-08-31T11:00:03Z')
 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.new_article, Article))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_article, Article))
 
-# if __name__ == '__main__':
-#     unittest.main()            
+if __name__ == '__main__':
+    unittest.main()            
