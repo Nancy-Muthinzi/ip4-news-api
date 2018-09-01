392332035a624be576a557287357ace7f33614ed
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-09-01 21:12:58 +0300

    process article results

--- app/request.py
+++ app/request.py
@@ -1,10 +1,10 @@
 from app import app
 import urllib.request , json
 from .models import source
-# from .models import article
+from .models import article
 
 Source = source.Source
-# Article = article.Article
+Article = article.Article
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
@@ -17,6 +17,7 @@ def get_source(category):
     Function to get json to respond to url request
     '''
     get_source_url=source_url.format(category, api_key)
+
     with urllib.request.urlopen(get_source_url) as url:
         get_source_data = url.read()
         get_source_response = json.loads(get_source_data)
@@ -69,50 +70,50 @@ def search_source(source_name):
 
             return search_source_results
 
-# #Getting article url
-# article = app.config['NEWS_API_ARTICLE_URL']
+#Getting article url
+article_url = app.config['NEWS_API_ARTICLE_URL']
 
-# def get_article(category):
-#     '''
-#     Function to get json to respond to url request
-#     '''
-#     get_article_url=article.format(category,api_key)
+def get_article(category):
+    '''
+    Function to get json to respond to url request
+    '''
+    get_article_url=article_url.format(category,api_key)
 
-#     with urllib.request.urlopen(get_article_url) as url:
-#         get_article_data = url.read()
-#         get_article_response = json.loads(get_article_data)
+    with urllib.request.urlopen(get_article_url) as url:
+        get_article_data = url.read()
+        get_article_response = json.loads(get_article_data)
 
-#         article_results = None
+        article_results = None
 
-#         if get_article_response['articles']:
-#             article_results_list = get_article_response['articles']
-#             article_results = process_results(article_results_list)
+        if get_article_response['articles']:
+            article_results_list = get_article_response['articles']
+            article_results = process_results(article_results_list)
 
-#     return article_results        
+    return article_results        
 
-# def process_results(article_results_list):
-#     '''
-#     This function processes the articles results and transfers them to a list of objects
+def process_results(article_results_list):
+    '''
+    This function processes the articles results and transfers them to a list of objects
     
-#     Args:
-#         article_list: dictionaties that contain article details
+    Args:
+        article_list: dictionaties that contain article details
 
-#     Returns:
-#         article_results: list of article objects
-#     '''
-#     article_results = []
-#     for article_item in article:
-#         id = article_item.get('id')
-#         name = article_item.get('name')
-#         author = article_item.get('author')
-#         title = article_item.get('title')
-#         description = article_item.get('description')
-#         url = article_item.get('url')
-#         urlToImage = article_item.get('urlToImage')
-#         publishedAt = article_item.get('publishedAt')
+    Returns:
+        article_results: article objects
+    '''
+    article_results = []
+    for article_item in article_results_list:
+        id = article_item.get('id')
+        name = article_item.get('name')
+        author = article_item.get('author')
+        title = article_item.get('title')
+        description = article_item.get('description')
+        url = article_item.get('url')
+        urlToImage = article_item.get('urlToImage')
+        publishedAt = article_item.get('publishedAt')
 
-#         if image:
-#             article_object = Article(id, name, author, title, description, url, urlToImage, publishedAt)
-#             article_results.append(article_object)
+        if urlToImage:
+            article_object = Article(id, name, author, title, description, url, publishedAt)
+            article_results.append(article_object)
 
-#         return article_results  
+        return article_results  
